721d9803d25e6184c566a1be775d7c09e78c0f8e
HossamSayed99 <hossamlion17@gmail.com>
2021-03-31 11:15:11 +0300

    Final Code

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -8,24 +8,25 @@ const ImageSlider = ({slides}) =>
         const [current, setCurrent] = useState(0);
         const length = slides.length;
 
-
+        // Moves The slider to Next Slide
         const nextSlide = () => {
             setCurrent((current+1) % length);
         }
+        // Moves the slider to Previous Slide
         const prevSlide = () =>{
             setCurrent((current-1 + length) %length);
         }
 
         /*
-            Scrolling Handling
+            Touch Swiping Handling
         */
             document.addEventListener('touchstart', handleTouchStart, false);        
             document.addEventListener('touchmove', handleTouchMove, false);
             
-            var xDown = null;                                                        
-            var yDown = null;
+            let xDown = null;                                                        
+            let yDown = null;
             
-            function getTouches(evt) {
+            const getTouches = (evt)  => {
               return evt.touches ||             // browser API
                      evt.originalEvent.touches; // jQuery
             }                                                     
@@ -41,28 +42,28 @@ const ImageSlider = ({slides}) =>
                 return;
             }
             
-                var xUp = evt.touches[0].clientX;                                    
-                var yUp = evt.touches[0].clientY;
-            
-                var xDiff = xDown - xUp;
-                var yDiff = yDown - yUp;
-            
-                if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-                    if ( xDiff > 0 ) {
-                        nextSlide();
-                    } else {
-                        prevSlide();
-                    }                       
-                }
-                /* reset values */
-                xDown = null;
-                yDown = null;                                             
+            let xUp = evt.touches[0].clientX;                                    
+            let yUp = evt.touches[0].clientY;
+        
+            let xDiff = xDown - xUp;
+            let yDiff = yDown - yUp;
+        
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+                if ( xDiff > 0 ) {
+                    nextSlide();
+                } else {
+                    prevSlide();
+                }                       
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
         };
 
 
         if(!Array.isArray(slides) || slides.length <= 0)
             return;
-
+        // Main Component
         return (
             <section className='slider'>
                 <div className='nav_bar'>
